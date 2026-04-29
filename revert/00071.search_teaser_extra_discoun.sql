523f786a0aaba96619083e74694f41fe236d809b
Sergey Yakovlev <fclaw007@gmail.com>
2026-04-29 10:55:42 +0300

    search teaser extra discount

---
+++ revert/00071.search_teaser_extra_discoun.sql
@@ -0,0 +1,7 @@
+-- Revert tkani-api:00071.search_teaser_extra_discoun from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ verify/00071.search_teaser_extra_discoun.sql
@@ -0,0 +1,7 @@
+-- Verify tkani-api:00071.search_teaser_extra_discoun on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
